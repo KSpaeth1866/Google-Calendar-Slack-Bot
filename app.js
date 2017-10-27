a4efb983bdb69692067445cfdd8198939057376d
Kobie <spaet062@umn.edu>
2017-10-27 13:14:28 -0700

    converted to await over .then

--- app.js
+++ app.js
@@ -22,45 +22,49 @@ app.get('/setup', (req, res) => {
   res.redirect(url)
 })
 
-app.get('/google/callback', (req, res) => {
-  let user;
-  User.findOne({slackId: req.query.state})
-  .then(u => {
-    user = u;
-    return google.getToken(req.query.code)
-  })
-  .then(tokens => {
+app.get('/google/callback', async (req, res) => {
+  try {
+    let user = await User.findOne({slackId: req.query.state});
+    let tokens = await google.getToken(req.query.code);
     user.google.tokens = tokens;
     user.google.isSetupComplete = true;
-    return user.save();
-  })
-  .then(saveResp => {
-    console.log('successful save');
-    res.status(200).json(saveResp)
-  })
-  .catch(err => {
-    console.log('error retrieving token: ', err)
-    res.status(500).json(err)
-  })
-})
-
-app.post('/slack/interactive', (req, res) => {
-  var payload = JSON.parse(req.body.payload);
-  if (payload.actions[0].value === 'true') {
-    User.findOne({slackId: payload.user.id})
-    .then(user => {
-      user.pending.time ?
-      google.createCalendarMeeting(user.google.tokens, user.pending.description, user.pending.date, user.pending.time, user.pending.invitees) :
-      google.createCalendarEvent(user.google.tokens, user.pending.description, user.pending.date)
-    })
-    .then(function() {
-      res.send('Created Reminder :white_check_mark:')
-    })
-    .catch(function(err) {
-      console.log('Error creating reminder', err);
-    });
+    await user.save();
+    res.status(200).send('You are now authenticated with Google! Thanks.');
   }
-  else res.send('Cancelled :x:')
+  catch (err) {
+    console.log('error retrieving token', err);
+    res.status(500).send("Error, please wait and try again");
+  }
+});
+
+// google.checkCalendarAvailability(user.google.tokens, user.pending.date, user.pending.time)
+// .then(resp => {
+//   console.log('checked availability response:\n', resp);
+//   console.log(resp.calendars['spaet062@umn.edu']);
+//   return google.createCalendarMeeting(user.google.tokens, user.pending.description, user.pending.date, user.pending.time, user.pending.invitees)
+// }) :
+
+app.post('/slack/interactive', async (req, res) => {
+  try {
+    let payload = JSON.parse(req.body.payload);
+    let user = await User.findOne({slackId: payload.user.id})
+    if (payload.actions[0].value === 'true') {
+      if (user.pending.time) await google.createCalendarMeeting(user.google.tokens, user.pending.description, user.pending.date, user.pending.time, user.pending.invitees)
+      else await google.createCalendarEvent(user.google.tokens, user.pending.description, user.pending.date)
+      user.pending = {invitees: [],};
+      await user.save();
+      res.send('Created Reminder :white_check_mark:')
+    }
+    else {
+      user.pending = {invitees: [],};
+      await user.save();
+      res.send('Cancelled :x:')
+    }
+  }
+  catch (err) {
+    console.log('error in slack interactive token', err);
+    res.status(500).send("Error, please wait and try again");
+  }
 })
 
 let port = process.env.PORT || 3000;
